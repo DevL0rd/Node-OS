992cdfdb33bd6ae8339a0e2fc2c486dda1dd8c71
DevL0rd <dmhzmxn@gmail.com>
2024-02-05 23:30:55 +0700

    purty

--- sys/shell.lua
+++ sys/shell.lua
@@ -1,3 +1,4 @@
+local termUtils = require("/lib/termUtils")
 term.setCursorPos(1, 1)
 term.clear()
 local shell = _G.shell
@@ -6,6 +7,17 @@ for i = 1, #paths do
     shell.setPath(paths[i] .. ":" .. shell.path())
 end
 shell.run("cd /home")
--- shell.run("shell.lua")
--- open the shell but clear the screen
-shell.run("shell.lua")
+local function main()
+    shell.run("shell.lua")
+end
+local function clear()
+    local file = fs.open("sys/ver.txt", "r")
+    ver = tonumber(file.readAll())
+    file.close()
+    termUtils.fillLine("-", 1, "cyan", "black")
+    termUtils.centerText("[ NodeOS Ver: " .. ver .. " ]", 1, "cyan", "black")
+    term.setBackgroundColor(colors.black)
+    term.setTextColor(colors.white)
+    term.setCursorPos(7, 2)
+end
+parallel.waitForAll(main, clear)
